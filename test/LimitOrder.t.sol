8b26de800431cd82f5ae5696ffd37e8b80e2ecbc
Joe Marron <marronjo@tcd.ie>
2024-02-21 08:34:47 +0000

    add limit order test script

---
+++ test/LimitOrder.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {LimitOrder} from "../src/LimitOrder.sol";
+
+contract LimitOrderTest is Test {
+
+}
